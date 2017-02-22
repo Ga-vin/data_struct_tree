158b73e184b53db74c3c21f2672836446fdd8207
Ga-vin <gavin_8724@163.com>
2017-02-22 18:58:18 -0500

    Add main function to project

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "common_types.h"
+
+int main(int argc, char *argv[])
+{
+    
+
+    return ;
+}
